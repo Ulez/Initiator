2460fe48dbe167958669868c68393f854e207b0f
Ulez <1104128773@qq.com>
2020-10-11 14:13:43 +0800

    提前在MultiDex之前加载sp，提前异步加载类。

--- app/src/main/java/fun/learnlife/initiator/MyApplication.java
+++ app/src/main/java/fun/learnlife/initiator/MyApplication.java
@@ -2,6 +2,7 @@ package fun.learnlife.initiator;
 
 import android.app.Application;
 import android.content.Context;
+import android.util.Log;
 
 import fun.learnlife.initiator.mytask.C1;
 import fun.learnlife.initiator.mytask.C10;
@@ -22,6 +23,7 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.i("lcy","MyApplication onCreate");
         Initiator.getInstance()
                 .addTask(new C23())
                 .addTask(new C22())
@@ -37,6 +39,21 @@ public class MyApplication extends Application {
                 .addTask(new C2())
                 .addTask(new C1())
                 .start();
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        Log.i("lcy","MyApplication attachBaseContext");
+//        提前加载SharedPreference
+//        MultiDex.install(this);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+//                提前异步类加载,需要加载哪些用替换系统ClassLoader打印出来。
+                new MainActivity();
+            }
+        }).start();
     }
 
     @Override
